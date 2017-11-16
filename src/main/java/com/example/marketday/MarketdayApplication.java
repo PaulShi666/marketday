441b6cfea2bb96b4533caad8d337682472f06fbb
ctshi <ctshi@sse.com.cn>
2017-11-16 10:58:43 +0800

    init

---
+++ src/main/java/com/example/marketday/MarketdayApplication.java
@@ -0,0 +1,12 @@
+package com.example.marketday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketdayApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/marketday/MarketdayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.marketday;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MarketdayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
