150e515209368cfd6c6cc34b75c7049a6fb44490
paulshi666 <1976727209@qq.com>
2017-11-20 20:24:00 +0800

    修正sql错误

--- marketDay.sql
+++ marketDay.sql
@@ -5,8 +5,11 @@ CREATE TABLE marketDay (
   dayId INT PRIMARY KEY AUTO_INCREMENT NOT NULL ,
   date  DATE NOT NULL ,
   des VARCHAR(20)
-)
+);
 
 USE trade;
 INSERT INTO marketDay VALUES(null,'2017-10-14','birthday');
+INSERT INTO marketDay VALUES(null,'2017-10-15','birthday');
+INSERT INTO marketDay VALUES(null,'2017-10-16','birthday');
+INSERT INTO marketDay VALUES(null,'2017-10-17','birthday');
 
